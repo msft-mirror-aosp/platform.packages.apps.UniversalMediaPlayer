760412eaac899535001af5af1515e35eb54a83f6
Christofer Åkersten <akersten@google.com>
2019-01-15 10:23:30 -0800

    Fix warnings for c-style arrays
    Test: Compile & lint
    Change-Id: I7cd8c3387ddd0eddb9c938c652f078f974762bb5

--- apps/Pump/java/com/android/pump/widget/SortOrderSpinner.java
+++ apps/Pump/java/com/android/pump/widget/SortOrderSpinner.java
@@ -71,7 +71,7 @@ public class SortOrderSpinner extends AppCompatSpinner {
     }
 
     private void initialize() {
-        String options[] = {
+        String[] options = {
             "name",
             "modified"
         };
